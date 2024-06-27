29b059fd4b86692728fe7a8d485a9a21140ce10c
Gabriel Ramos <bielrammos@gmail.com>
2024-06-27 20:34:36 -0300

    - Creating one test case - Update browser to support multi windows - Update widget to support more attributes and stable

--- controllers/browser/browser_controller.py
+++ controllers/browser/browser_controller.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from typing import List
 
 
 class BrowserController:
-    def __init__(self, browser_type: str = 'firefox') -> None:
+    def __init__(self, browser_type: str = 'firefox', maximize=False) -> None:
         self.browser_type = browser_type.lower()
+        self.maximize = maximize
         self.driver = self._initialize_driver()
 
     def _initialize_driver(self) -> WebDriver:
@@ -14,8 +18,8 @@ class BrowserController:
             driver = webdriver.Firefox()
         else:
             raise ValueError(f"Unsupported browser type: {self.browser_type}")
-
-        driver.maximize_window()
+        if self.maximize:
+            driver.maximize_window()
         return driver
 
     def open_url(self, url: str) -> None:
@@ -23,3 +27,19 @@ class BrowserController:
 
     def close_browser(self) -> None:
         self.driver.quit()
+
+    def get_current_window_handle(self) -> str:
+        return self.driver.current_window_handle
+
+    def get_all_window_handles(self) -> List[str]:
+        return self.driver.window_handles
+
+    def switch_to_window(self, window_handle: str) -> None:
+        self.driver.switch_to.window(window_handle)
+
+    def switch_to_new_window(self, timeout: int = 10) -> None:
+        all_windows = self.get_all_window_handles()
+
+
+    def close_current_window(self) -> None:
+        self.driver.close()
---
+++ projects/example_project/example_suite/test_example_one.py
@@ -0,0 +1,33 @@
+from hextec.controllers.browser.browser_controller import BrowserController
+from hextec.controllers.browser.widget import Widget
+
+def test_example_one():
+    browser = BrowserController()
+    browser.open_url("https://www.linkedin.com/")
+
+    #btn_login = Widget(browser.driver, data_tracking_will_navigate="Sign in")
+    btn_login = Widget(browser.driver, text="Sign in")
+    btn_username_login = Widget(browser.driver, id="username", name="session_key", type="text")
+    btn_password_login = Widget(browser.driver, id="password", name="session_password", type="password")
+    btn_login.click()
+    btn_username_login.click()
+    btn_username_login.enter_text("*@gmail.com")
+    btn_password_login.click()
+    btn_password_login.enter_text("*")
+    btn_login = Widget(browser.driver, text="Sign in", type="submit")
+    btn_login.click()
+
+    search = Widget(browser.driver, placeholder="Search", type="text")
+    search.click(timeout=30)
+    search.enter_text("Empreendedorismo")
+    search_input = search.get_input
+    search_input.press_enter()
+
+    #btn_people = Widget(browser.driver, class_="search-navigation", text="People")
+    #btn_people.click()
+    btn_see_all_people = Widget(browser.driver, text="See all people results")
+    btn_see_all_people.scroll_to()
+    btn_see_all_people.click()
+
+
+    print(Widget.get_all_elements_with_attribute(browser.driver, "href"))
