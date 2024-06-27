eae0ea450644cf755986d37292e1b4cceba02e95
Gabriel Ramos <bielrammos@gmail.com>
2024-06-27 20:34:36 -0300

    - Creating one test case - Update browser to support multi windows - Update widget to support more attributes and stable

--- controllers/browser/widget.py
+++ controllers/browser/widget.py
@@ -1,14 +1,16 @@
+import re
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
-from typing import List, Optional, Any, Dict
+from typing import Any, Dict
+from .input import Input
+
 
 class Widget:
-    def __init__(self, driver: WebDriver, text: Optional[str] = None, **kwargs: str) -> None:
+    def __init__(self, driver: WebDriver, **kwargs: str) -> None:
         self.driver = driver
-        self.text = text
         self.attrs = kwargs
         self.xpath = self._build_xpath()
 
@@ -16,20 +18,44 @@ class Widget:
         xpath = "//*"
         conditions = []
 
-        if self.text:
-            conditions.append(f"text()='{self.text}'")
         for attr, value in self.attrs.items():
-            conditions.append(f"@{attr}='{value}'")
+            attr = re.sub(r'_', '-', attr)
+            if attr == 'text':
+                conditions.append(f"contains(text(), '{value}')")
+            elif 'class' in attr:
+                conditions.append(f"@class='{value}'")
+            else:
+                conditions.append(f"@{attr}='{value}'")
+
+        if conditions:
+            xpath += "[" + " and ".join(conditions) + "]"
+        print(xpath)
+        return xpath
+
+    def _build_fallback_xpath(self) -> str:
+        xpath = "//*"
+        conditions = []
+
+        for attr, value in self.attrs.items():
+            attr = re.sub(r'_', '-', attr)
+            if attr == 'text':
+                conditions.append(f"contains(text(), '{value}')")
+            else:
+                conditions.append(f"contains(@{attr}, '{value}')")
 
         if conditions:
             xpath += "[" + " and ".join(conditions) + "]"
 
         return xpath
 
-    def find(self, timeout: int = 10) -> WebElement:
-        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.XPATH, self.xpath)))
+    def find(self, timeout=10):
+        try:
+            return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.XPATH, self.xpath)))
+        except:
+            fallback_xpath = self._build_fallback_xpath()
+            return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.XPATH, fallback_xpath)))
 
-    def click(self, timeout: int = 10) -> None:
+    def click(self, timeout=10):
         element = self.find(timeout)
         element.click()
 
